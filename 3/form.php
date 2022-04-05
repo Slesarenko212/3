a0c04e9ab80dcf3905ec31dd80abb42cd37040d5
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-04-05 15:42:34 +0300

    Update form.php

--- 3/form.php
+++ 3/form.php
@@ -1,6 +1,5 @@
-<
   header('Content-Type: text/html; charset=UTF-8');
-  form action="" method="POST">
+< form action="" method="POST">
   <input name="fio" />
   <input type="submit" value="ok" />
 </form>
