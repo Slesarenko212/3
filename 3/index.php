2dfb7b3a8d718789c243f67614d03c5fba22e9ad
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-05-05 19:19:26 +0300

    Update index.php

--- 3/index.php
+++ 3/index.php
@@ -1,74 +1,144 @@
 <?php
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
-header('HTTP/1.1 404 Not Found');
+
+function goBack(){
+    print('<div style="color:#dc3555; background-color:#212530;">Неверно введены суперспособности </div>');
+    include('form.php');
+    exit();
+}
+
 header('Content-Type: text/html; charset=UTF-8');
 
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
-  if (!empty($_GET['save'])) {
-    // Если есть параметр save, то выводим сообщение пользователю.
-    print('Спасибо, результаты сохранены.');
-  }
-  // Включаем содержимое файла form.php.
-  include('form.php');
-  // Завершаем работу скрипта.
-  exit();
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_GET['save'])) {
+        print('<div style="color:#198759; background-color:#212530;">Данные успешно сохранены! </div>');
+    }
+    include('form.php');
+    exit();
+}else {
+    $errors = FALSE;
+    $errors_string = array();
+    for($i=0; $i<9; $i++){
+        $errors_string[$i]='';
+    }
+
+    if (empty($_POST['userName'])) {
+        $errors_string[0] = '<div style="color:#dc3555; background-color:#212530;">Введите Ваше имя </div>';
+        $errors = TRUE;
+    }
+    if (empty($_POST['userEmail'])) {
+        $errors_string[1] = '<div style="color:#dc3555; background-color:#212530;">Введите email </div>';
+        $errors = TRUE;
+    } else if (!preg_match("/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/", $_POST['userEmail'])) {
+        $errors_string[2]='<div style="color:#dc3549; background-color:#212530;">Неверно введён email</div>';
+        $errors = TRUE;
+    }
+    if (empty($_POST['userBirthdate'])) {
+        $errors_string[3]=('<div style="color:#dc3555; background-color:#212530;">Укажите дату рождения</div>');
+        $errors = TRUE;
+    } else if (!preg_match("(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8])))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29))", $_POST['userBirthdate'])) {
+        $errors_string[4]=('<div style="color:#dc3555; background-color:#212530;">Неверно указана дата рождения </div>');
+        $errors = TRUE;
+    }
+    if (empty($_POST['userGender'])) {
+        $errors_string[5]=('<div style="color:#dc3555; background-color:#212530;">Укажите Ваш пол </div>');
+        $errors = TRUE;
+    } else if ($_POST['userGender'] != 1 && $_POST['userGender'] != 2) {
+        $errors_string[6]=('<div style="color:#dc3555; background-color:#212530;">Неверно указан пол </div>');
+        $errors = TRUE;
+    }
+
+    if (empty($_POST['userLimbs'])) {
+        $errors_string[7]=('<div style="color:#dc3555; background-color:#212530;">Укажите количество конечностей </div>');
+        $errors = TRUE;
+    } else if ($_POST['userLimbs'] != 1 && $_POST['userLimbs'] != 2 && $_POST['userLimbs'] != 3) {
+        $errors_string[8]=('<div style="color:#dc3555; background-color:#212530;">Неверно вуказано число конечностей</div>');
+        $errors = TRUE;
+    }
+
+    if ($errors) {
+        for($i=0; $i<9; $i++){
+            print($errors_string[$i]);
+        }
+        include('form.php');
+        exit();
+    }
+
+    $userName = $_POST['userName'];
+    $userEmail = $_POST['userEmail'];
+    $userBirthdate = $_POST['userBirthdate'];
+    $userGender = $_POST['userGender'];
+    $userLimbs = $_POST['userLimbs'];
+    $userBio = $_POST['userBio'];
+
+
+    $userAb = array();
+
+    if (!empty($_POST['ab'])) {
+        $n = count($_POST['ab']);
+        $abCheck = array(true, true, true);
+        for ($i = 0; $i < $n; $i++) {
+            if ($_POST['ab'][$i] == 0 || $_POST['ab'][$i] == 1 || $_POST['ab'][$i] == 2) {
+                if ($_POST['ab'][$i] == 0 && $abCheck[0]) {
+                    $abCheck[0] = false;
+                } else if ($_POST['ab'][$i] == 1 && $abCheck[1]) {
+                    $abCheck[1] = false;
+                } else if ($_POST['ab'][$i] == 2 && $abCheck[2]) {
+                    $abCheck[2] = false;
+                } else {
+                    goBack();
+                }
+            } else {
+                goBack();
+            }
+        }
+        for ($i = 0; $i < 3; $i++) {
+            if (!$abCheck[$i]) {
+                $userAb[$i] = 1;
+            } else {
+                $userAb[$i] = 0;
+            }
+        }
+    } else {
+        for ($i = 0; $i < 3; $i++) {
+            $userAb[$i] = 0;
+        }
+    }
+
+
+    try {
+        $user = 'u47558';
+        $pass = '3872701';
+        $db = new PDO('mysql:host=localhost;dbname=u47558', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+        $stmt_1 = $db->prepare("INSERT INTO user_data (name, email, birthdate, gender, limbs, bio) VALUES (:name, :email, :date, :gender, :limbs, :bio)");
+        $stmt_1->bindParam(':name', $userName);
+        $stmt_1->bindParam(':email', $userEmail);
+        $stmt_1->bindParam(':date', $userBirthdate);
+        $stmt_1->bindParam(':gender', $userGender);
+        $stmt_1->bindParam(':limbs', $userLimbs);
+        $stmt_1->bindParam(':bio', $userBio);
+        $db->beginTransaction();
+        $stmt_1->execute();
+        $id = $db->lastInsertId();
+        $db->commit();
+
+
+        //$get_id = $db->prepare("select max(id) from user_data");
+        //$get_id->execute();
+        //$id = $get_id->fetchColumn();
+
+
+        $stmt_2 = $db->prepare("INSERT INTO user_ab (user_data_id, god, reader, levitation) VALUES (:id, :god, :reader, :levitation)");
+        $stmt_2->bindParam(':id', $id);
+        $stmt_2->bindParam(':god', $userAb[0]);
+        $stmt_2->bindParam(':reader', $userAb[1]);
+        $stmt_2->bindParam(':levitation', $userAb[2]);
+        $db->beginTransaction();
+        $stmt_2->execute();
+        $db->commit();
+
+    } catch (PDOException $e) {
+        print('Error : ' . $e->getMessage());
+        exit();
+    }
+    header('Location: ?save=1');
 }
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-
-// Проверяем ошибки.
-$errors = FALSE;
-if (empty($_POST['fio'])) {
-  print('Заполните имя.<br/>');
-  $errors = TRUE;
-}
-
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
-
-if ($errors) {
-  // При наличии ошибок завершаем работу скрипта.
-  exit();
-}
-
-// Сохранение в базу данных.
-
-$user = 'db';
-$pass = '123';
-$db = new PDO('mysql:host=localhost;dbname=test', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-
-// Подготовленный запрос. Не именованные метки.
-try {
-  $stmt = $db->prepare("INSERT INTO application (name) SET name = ?");
-  $stmt -> execute(array('fio'));
-}
-catch(PDOException $e){
-  print('Error : ' . $e->getMessage());
-  exit();
-}
-
-//  stmt - это "дескриптор состояния".
- 
-//  Именованные метки.
-//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
-//$stmt -> execute(array('label'=>'perfect', 'color'=>'green'));
- 
-//Еще вариант
-/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$firstname = "John";
-$lastname = "Smith";
-$email = "john@test.com";
-$stmt->execute();
-*/
-
-// Делаем перенаправление.
-// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
-// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
-header('Location: ?save=1');
