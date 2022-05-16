bee10a0fb766bd0fee808338512b6d85b745cd88
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-05-16 20:49:45 +0300

    Delete java.js

--- 3/java.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function (){
-    $('#check').change((event)=>{
-        if($('#check').is(":checked")){
-            $('#submitButton').prop("disabled", false)
-        }else{
-            $('#submitButton').prop("disabled", true);
-        }
-    });
-});
